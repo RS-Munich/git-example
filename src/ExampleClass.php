396717156ee6aca8e59d4ca0109705caf2317ac3
Juho Jaakkola <juho.jaakkola@xentral.com>
2020-11-05 11:25:32 +0100

    Adds AnotherClass service for Example process

--- src/ExampleClass.php
+++ src/ExampleClass.php
@@ -2,6 +2,12 @@
 
 class ExampleClass {
 
+    private $anotherClass;
+
+    public function __construct(AnotherClass $anotherClass) {
+        $this->anotherClass = $anotherClass;
+    }
+
     /**
      * Does something.
      */
